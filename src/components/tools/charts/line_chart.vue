45854189e97d564f3bbacf4352ee3e3a85fe2d7b
hector237 <85283152+hector237@users.noreply.github.com>
2021-06-06 04:34:23 +0200

    add chart and calendar

---
+++ src/components/tools/charts/line_chart.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-card elevation="4" shaped>
+        <v-card-title>Consumo de energia</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>
+          <v-sparkline
+            color="error"
+            height="100"
+            padding="12"
+            stroke-linecap="round"
+            smooth="0"
+            auto-draw
+            auto-draw-easing="ease"
+            line-width="0.9"
+            :labels="values"
+            :value="values"
+          >
+          </v-sparkline>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-card-text
+            ><p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
+              iste est natus accusamus!
+            </p></v-card-text
+          >
+          <v-btn depressed color="primary" fab>
+            <v-icon>arrow_right_alt</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "lineChart",
+
+  data() {
+    return {
+      values: [1, 2, 66, 1, 55, 88, 44, 99, 33, 55, 99, 11, 2],
+    };
+  },
+
+  components: {},
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -53,13 +53,45 @@
         </v-card>
       </v-col>
     </v-row>
+    <!-- charts -->
+    <v-row>
+      <v-col cols="12" md="" lg="6">
+        <lineChart />
+      </v-col>
+
+      <v-col cols="12" md="" lg="6">
+        <lineChart />
+      </v-col>
+    </v-row>
+    <!-- end charts -->
+    <!-- calendar - task -->
+    <v-row>
+      <!-- task -->
+      <v-col cols="12" md="4" lg="6">
+        <lineChart />
+      </v-col>
+      <!-- end task -->
+      <!-- calendar -->
+      <v-col class="d-none d-lg-block" cols="12" md="8" lg="6">
+        <calendar />
+      </v-col>
+
+      <!-- end calendar -->
+    </v-row>
+    <!-- end calendar - task -->
   </v-container>
 </template>
 
 <script>
+import lineChart from "@/components/tools/charts/line_chart";
+import calendar from "@/components/tools/calendar/calendar";
+
 export default {
   name: "Home",
 
-  components: {},
+  components: {
+    lineChart,
+    calendar,
+  },
 };
 </script>
